f833ad2ecce4a7fdac5cc473356ecd0748214f63
Faizaan Khan <faizaankhan24@gmail.com>
2023-01-11 20:04:20 +0530

    Adding models for the app

---
+++ Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,8 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by Faizaan Khan on 1/11/23.
+//
+
+import Foundation
---
+++ Moonshot/Color-Theme.swift
@@ -0,0 +1,8 @@
+//
+//  Color-Theme.swift
+//  Moonshot
+//
+//  Created by Faizaan Khan on 1/11/23.
+//
+
+import Foundation
